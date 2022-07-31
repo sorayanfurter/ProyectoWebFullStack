26a2b06c42c424f420c1550a27145572ed513fe3
Soraya <sorinfurter@gmail.com>
2022-07-31 11:18:19 -0300

    +

--- ProyectoIntegrador/Backend/src/main/java/com/portfolio/sorayafp/Controller/UsuarioController.java
+++ ProyectoIntegrador/Backend/src/main/java/com/portfolio/sorayafp/Controller/UsuarioController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/usuario")
-@CrossOrigin(origins = "https://frontendsorayaf.web.app/")
+@CrossOrigin(origins = "https://frontendsorayaf.web.app")
 
 public class UsuarioController {
 
